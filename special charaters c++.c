c79ccbb30874a0b63ab0bb3fc8b714d81ad093a5
RoshniSingh2068 <roshnisingh1228@gmail.com>
2024-08-26 11:09:23 +0530

    Create special charaters c++.c

---
+++ special charaters c++.c
@@ -0,0 +1,11 @@
+// the sequence \" insert the double quotes in the string
+
+#include <iostream>
+
+using namespace std;
+
+int main (){
+    string txt = "we are the so called \" viking \" from the north"; //will print the ("") inside word as it is
+    cout<<txt;
+    return 0;
+}
